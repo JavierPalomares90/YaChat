d11eaa4f780a3ed3be8afd32bf9e423d093b17aa
Javier Palomares <javier.palomares.90@gmail.com>
2019-02-10 13:35:05 -0600

    Program is hanging at tcp_socket receive

--- client/YaChat.py
+++ client/YaChat.py
@@ -3,15 +3,78 @@
 # Implementation of YaChat client for first Iteration
 # EE 382N, Spring 2019
 #--------------------------------#
-# TODO: Add requirements file
 
 import argparse
-parser = argparse.ArgumentParser()
-parser.parse_args()
+import socket
 
 
+# returns a tcp socket at the given host and port
+def get_tcp_socket(host, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    if s:
+        try:
+            server_address = (host, port)
+            s.connect(server_address)
+        except Exception as e:
+            print(e)
+            raise e
+    return s
 
 
+def get_udp_socket(host, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # Bind the socket to a port
+    server_address = (host, port)
+    if sock:
+        try:
+            sock.bind(server_address)
+        except Exception as e:
+            print(e)
+            raise e
+    return sock
+
+
+def get_ip_address():
+    # TODO: Fix implemenation
+    return "127.0.0.1"
+
+
+def get_helo_msg(screen_name, udp_port):
+    msg = "HELO "
+    msg += screen_name
+    msg += " "
+    ip_address = get_ip_address()
+    msg += " "
+    msg += ip_address
+    msg += " "
+    msg += str(udp_port)
+    msg +="\n"
+    return msg
+
+
+# Initialize the connection with the server
+def init_connection(screen_name, host_name, tcp_port):
+    # create a tcp socket to connect to the server
+    tcp_socket = get_tcp_socket(host_name, tcp_port)
+    if not tcp_socket:
+        raise Exception("Unable to initialize tcp connection with server at {}:{}".format(host_name,tcp_port))
+    # create a udp socket to listen for messages.
+    # Pass in 0 as the port to let the OS pick the port
+    udp_socket = get_udp_socket(host_name, 0)
+    if not udp_socket:
+        raise Exception("Unable to initialize udp connection with server at {}".format(host_name))
+
+    # Get the HELO msg to send to the server
+    helo_msg = get_helo_msg(screen_name, udp_socket.getsockname()[1])
+
+    if helo_msg:
+        try:
+            tcp_socket.send(bytes(helo_msg, "utf8"))
+        except Exception as e:
+            print(e)
+            raise(e)
+    msgFromServer = tcp_socket.recv(2048)
+    print(msgFromServer)
 
 
 # main method
@@ -19,11 +82,15 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("screen_name", help="Your screen name")
     parser.add_argument("host_name", help="The server's hostname")
-    parser.add_argument("tcp_port", help="The server's welcome tcp port")
+    parser.add_argument("tcp_port", help="The server's welcome tcp port",type=int)
 
+    # parse the arguments
     args = parser.parse_args()
-    screen_name = args.screen_name
-    host_name = args.host_name
-    tcp_port = args.tcp_port
+    screenName = args.screen_name
+    hostName = args.host_name
+    tcpPort = args.tcp_port
+
+    # Initialize the connection
+    init_connection(screenName,hostName,tcpPort)
 
 
