f280578061be7be2d769b23a4b45653aad2faaf2
Javier Palomares <javier_palomares@homedepot.com>
2019-02-15 20:03:02 -0600

    new ip address works on mac

--- client/chatter/Chatter.py
+++ client/chatter/Chatter.py
@@ -94,7 +94,8 @@ class Chatter:
     def get_ip_address(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.connect(("8.8.8.8", 80))
-        ip_address = s.getsockname()[0]
+        self.ip_address = s.getsockname()[0]
+        s.close()
         return self.ip_address
 
     def get_msg_helo(self):
