adbd681495f4005715a048462b12f2481684e2a9
Javier Palomares <javier.palomares.90@gmail.com>
2019-02-15 20:06:39 -0600

    fixed bug

--- client/chatter/Chatter.py
+++ client/chatter/Chatter.py
@@ -96,7 +96,7 @@ class Chatter:
         s.connect(("8.8.8.8", 80))
         ip_address = s.getsockname()[0]
         s.close()
-        return self.ip_address
+        return ip_address
 
     def get_msg_helo(self):
         msg = "HELO "
