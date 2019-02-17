a7b48fd60c346d0660105f0a4bfda1828c05300f
Javier Palomares <javier.palomares.90@gmail.com>
2019-02-17 13:18:07 -0600

    Revert "start threads as daemons"
    This reverts commit 3346f7d9f0dd0084f33b3b4aaf50fad795f6fa54.

--- client/YaChat.py
+++ client/YaChat.py
@@ -35,8 +35,6 @@ def main():
     receive = ReceiveThread(chatter, BUFFER_SIZE)
     send = SendThread(chatter)
 
-    receive.daemon = True
-    send.daemon = True
     receive.start()
     send.start()
 
